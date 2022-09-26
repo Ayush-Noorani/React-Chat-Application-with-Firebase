d84f8e763d0c9da8c2c98472e2d5afe806006d9d
Ayush <ayushnoorani6@gmail.com>
2022-09-26 18:23:19 +0530

    Tried adding user info in firestore database

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import {React , useState} from 'react';
-import { auth , storage } from '../firebase'
+import { auth , storage , db } from '../firebase'
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { doc, setDoc } from 'firebase/firestore';
 
 const Register = () => {
 
@@ -14,27 +15,44 @@ const Register = () => {
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-  try {
-    const resp = await createUserWithEmailAndPassword(auth, email, password)
-    const storageRef = ref(storage, displayName)
-    const uploadTask = uploadBytesResumable(storageRef, file)
+    try {
+      const resp = await createUserWithEmailAndPassword(auth, email, password) //this saves the email and password in firestore
+      const storageRef = ref(storage, displayName)
+      const uploadTask = uploadBytesResumable(storageRef, file).catch(err => console.log(err));
 
-    uploadTask.on(
-      (error) => {
-        setErr(true)
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) =>{
-          await updateProfile(resp.user, {displayName, photoURL:downloadURL})
-        })
-      }
-    )
+      uploadTask.on(
+        (error) => {
+          setErr(true)
+        },
+        () => {
+          // this stores the name and photo
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) =>{
+            await updateProfile(resp.user, {displayName, photoURL:downloadURL})
 
-  } catch (error) {
-    setErr(true)
+            //this stores the users information in a folder inside firestore storage
+
+            const userRef = doc(db, 'users', resp.user.uid);
+            await setDoc(userRef)
+            // db.collection('users').add({
+            //   uid : resp.user.uid,
+            //   displayName: resp.user.displayName,
+            //    email: resp.user.email,
+            //    photoURL: downloadURL
+            // })
+            // await setDoc(doc(db, "/users", resp.user.uid), {
+            //   displayName: resp.user.displayName,
+            //   email: resp.user.email,
+            //   photoURL: downloadURL
+            // });
+          });
+        }
+      );
+    }
+    catch (error) {
+      setErr(true)
+    }
   }
-}
-
+  
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
